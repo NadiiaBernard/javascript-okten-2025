ea2126d28ff6e0eaae7cebf2a5741da80f689494
NadiiaBernard <nadiia.brn@gmail.com>
2025-09-25 23:43:57 +0300

    add cw1.3

---
+++ Lectures/Lesson1/Less1-3/main.js
@@ -0,0 +1,28 @@
+console.log('qwerty');
+
+// number
+
+// string
+
+// boolean
+
+// var a;
+// a = 'hello';
+
+// {
+// let a = 'hello okten';
+// var b = 'hello world';
+//
+// }
+//
+// console.log(a);
+// console.log(b);
+
+
+const PI = 3.14;
+console.log(PI);
+
+let a = 'sdsdasda';
+console.log(a);
+a = 'okten';
+console.log(a);
