a08e018582bc2fe5ed81eea45652b7ef84f1ec16
NadiiaBernard <nadiia.brn@gmail.com>
2025-10-06 22:25:23 +0300

    add cw1.5

---
+++ Lectures/Lesson1/Less1-4/main1-4.js
@@ -0,0 +1,9 @@
+let a = 'hello okten';
+console.log(a);
+console.log(typeof a, a);
+
+let b = 100500;
+console.log(typeof b, b);
+
+let c = true;
+console.log(typeof c, c);
---
+++ Lectures/Lesson1/Lesson1-5/main1-5.js
@@ -0,0 +1,17 @@
+let a = 'qwerty';
+let b = "\'qwerty\'";
+
+console.log(a, b);
+
+let c = `okten`;
+console.log(c)
+
+let greeting = 'hello';
+let intro = 'my name is Nadya';
+
+let msg = greeting + ' ' + intro;
+console.log(msg);
+
+let msg2 = `${greeting} ${intro} asd`;
+console.log(msg2);
+
