1bf45c789e5c957e78c322f3009a175355fa37af
NadiiaBernard <nadiia.brn@gmail.com>
2025-10-08 17:47:52 +0300

    add cw1.9

---
+++ Lectures/Lesson1/Less1-6/main1-6.js
@@ -0,0 +1,23 @@
+let a = 100500;
+console.log(a);
+
+let b = 3.1234;
+console.log(b);
+
+console.log(10 + 20);
+console.log(10 - 20);
+console.log(10 * 20);
+console.log(10 / 20);
+
+
+console.log(11 % 3);
+
+console.log(0.1 + 0.2);
+
+let big = 1223282988382392392939230n;
+let big2 = BigInt(1223282988382392392939230);
+console.log(big, big2);
+
+let n1 = 100;
+let n2 = 200;
+console.log(n1 + n2);
---
+++ Lectures/Lesson1/Less1-8/main1-8.js
@@ -0,0 +1,25 @@
+console.log(100500 + '');
+console.log(typeof (100500 + ''));
+
+console.log(100 + 200 + '');
+console.log('' + 100 + 200);
+
+console.log(+'100500');
+console.log(+'100500asd');
+console.log(parseInt('100500asd'));
+
+
+console.log(!!1000);
+console.log(!!-1000);
+console.log(!!0);
+
+console.log(!!'asd');
+console.log(!!'');
+
+console.log(!!'0');
+
+console.log(true + '');
+console.log(+true);
+console.log(+false);
+console.log(100 + false);
+console.log(100 + true);
