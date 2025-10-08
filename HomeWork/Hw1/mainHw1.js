b63fb910bdec2116013ad75e5ddf207da6ad4021
NadiiaBernard <nadiia.brn@gmail.com>
2025-10-08 21:59:48 +0300

    add hw1

---
+++ HomeWork/Hw1/mainHw1.js
@@ -0,0 +1,55 @@
+// #dYQNrBV
+// Створити змінні. Присвоїти кожному з них значення: ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+// let a= 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// console.log(a, b, c, d);
+//
+// let n1 = 1;
+// let n2 = 10;
+// let n3 = -999;
+// let n4 = 123;
+// const PI = 3.14;
+// let n5 = 2.7;
+// let n6 = 16;
+// console.log(n1, n2, n3, n4, n5, n6, PI);
+//
+// let bool1 = true;
+// let bool2 = false;
+// console.log(bool1, bool2);
+
+
+
+
+// #6Qb97gsv
+// – Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З’єднати їх в одну змінну person (Не об’єкт, просто за допомогою конкатенації)
+// let firstName = 'Nadiia';
+// let lastName = 'Feniuk';
+// let middleName = 'Stepanivna';
+// let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person);
+
+
+
+
+// #4N0y5tufA
+// – За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+
+
+// #ruUtWDUI
+// Додаткове для тих, хто цікавився prompt`oм
+// – За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
+// let firstName = prompt('Як тебе звати?');
+// let middleName = prompt('Яке в тебе по батькові?');
+// let age = prompt('Скільки тобі років?');
+// console.log(firstName, middleName, +age);
