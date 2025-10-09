e61fa0b938df3aced8b67abed5d749370bef684b
NadiiaBernard <nadiia.brn@gmail.com>
2025-10-09 23:52:11 +0300

    add cw2.1

---
+++ Lectures/Lesson2/Less2-1/main2-1.js
@@ -0,0 +1,19 @@
+let arr;
+arr = [123, 526, -55, 0, true, 'qwerty'];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[3]);
+
+console.log(arr.length);
+
+arr[1] = 'new value';
+console.log(arr);
+
+arr[6] = 1;
+console.log(arr);
+arr[arr.length] = 'okten';
+console.log(arr);
+arr[arr.length] = 12345;
+console.log(arr);
+
+
