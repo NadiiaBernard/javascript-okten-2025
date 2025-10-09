3db1ba102972a61658d23e58e8255c9172c0278a
NadiiaBernard <nadiia.brn@gmail.com>
2025-10-10 00:15:08 +0300

    add cw2.2

---
+++ Lectures/Lesson2/Less2-2/main2-2.js
@@ -0,0 +1,13 @@
+let arr = [
+    [11, 22, 63],
+    [],
+    []
+];
+console.log(arr);
+console.log(arr[0]);
+
+let innerArray = arr[0];
+console.log(innerArray);
+console.log(innerArray[1]);
+
+console.log(arr[0][1]);
