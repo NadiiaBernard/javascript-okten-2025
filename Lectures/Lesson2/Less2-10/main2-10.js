bd7461a19cd7bbd1eedd5ee463c5804a60724368
NadiiaBernard <nadiia.brn@gmail.com>
2025-10-22 01:31:08 +0300

    add cw2.10

---
+++ Lectures/Lesson2/Less2-10/main2-10.js
@@ -0,0 +1,19 @@
+let obj = {
+    id: 1,
+    name: 'vasya',
+    age: 25
+}
+
+// let access;
+// let condition = obj.age > 18;
+//
+// if (obj.age > 18) {
+//     access = true;
+// } else {
+//     access = false;
+// }
+// console.log(access);
+
+
+let access = obj.age > 18 ? 'yes' : 'no';
+console.log(access);
