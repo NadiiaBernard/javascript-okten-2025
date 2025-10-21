c84c4c233554acecdc0095c153fbc6a189e81d18
NadiiaBernard <nadiia.brn@gmail.com>
2025-10-21 21:31:34 +0300

    add cw2.7

---
+++ Lectures/Lesson2/Less2-7/main2-7.js
@@ -0,0 +1,12 @@
+let a = 100;
+let b = a;
+b = b + 10;
+console.log(b);
+console.log(a);
+
+
+let user = {name: 'vasya'};
+let user2 = user;
+user2.age = 31;
+console.log(user2);
+console.log(user);
