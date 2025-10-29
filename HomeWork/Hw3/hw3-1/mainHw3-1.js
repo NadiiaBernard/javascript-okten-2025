e44cda84da2c1febdf4cbc53fc752fdc9ab4f17c
NadiiaBernard <nadiia.brn@gmail.com>
2025-10-30 01:02:56 +0200

    add hw3

---
+++ HomeWork/Hw3/hw3-1/mainHw3-1.js
@@ -0,0 +1,57 @@
+// #y7crMeFwHcS
+// – За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// for (let i = 0; i < 10; i++) {
+//     document.write('<div>okten2025</div>');
+// }
+
+
+// #TYj7ncx
+// – За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+// for (let i = 0; i < 10; i++) {
+//     document.write(`<div>okten 2025 - ${i}</div>`)
+// }
+
+
+// #uzkt71dp
+// – За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// let i = 0;
+// while (i<20){
+//     document.write(`<h1>while</h1>`);
+//     i++;
+// }
+
+
+// #OeT7t3uUMFi
+// – За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+// let i = 1;
+// while (i<21){
+//     document.write(`<h1>while - ${i}</h1>`);
+//     i++;
+// }
+
+
+// #vLSZKMlO
+// – Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру за шаблоном
+// Масив:
+//     let listOfItems = [‘html’, ‘css’, ‘javascript’, ‘mysql’, ‘mongodb’, ‘react’, ‘angular’, ‘node.js’];
+//
+// ШАБЛОН:
+//     <ul>
+//         <li>ITEM OF ARRAY</li>
+//         <!–
+//         і тд інші об’єкти масиву
+//         …
+//         –>
+//     </ul>
+// замість ‘ITEM OF ARRAY’ підставити елемент з масиву, щоб вийшов цілий список з даними з масиву
+
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// document.write(`<ul>`);
+// for (let listOfItem of listOfItems) {
+//     document.write(`<li>${listOfItem}</li>`);
+// }
+// document.write(`</ul>`);
+
+
+
