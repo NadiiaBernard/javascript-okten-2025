72b1a501c5d11921c7f0b3f1429ad8377788e939
NadiiaBernard <nadiia.brn@gmail.com>
2025-11-11 20:34:34 +0200

    add hw4-2

---
+++ HomeWork/Hw4/hw4-2/mainHw4-2.js
@@ -0,0 +1,15 @@
+// #mkGDenYnNjn.html
+// Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
+
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+    for (let i = 0; i < currencyValues.length; i++) {
+        if (currencyValues[i].currency === exchangeCurrency) {
+            return sumUAH / currencyValues[i].value;
+        }
+    }
+    return ('error');
+}
+
+console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'USD'));
+console.log(exchange(12000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'EUR'));
