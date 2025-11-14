e4ff1fc367f99c91320e907e02a07571281c8615
NadiiaBernard <nadiia.brn@gmail.com>
2025-11-14 22:49:02 +0200

    add cw5-2

---
+++ Lectures/Lesson5/Less5-2/main5-2.js
@@ -0,0 +1,12 @@
+// function calc(a, b) {
+//     return a + b;
+// }
+
+// let calc = (a,b) => a+b;
+// console.log(calc(10, 20));
+
+// let asd = (a) => console.log('hi');
+// console.log(asd());
+
+// let qwe = () => console.log(arguments);
+// console.log(qwe());
